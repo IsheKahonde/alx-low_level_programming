bb4a941f622c74f40f0bf12efc1c4fd0776578c4
IsheKahonde <ishekahonde@gmail.com>
2022-03-25 17:57:55 +0200

    Printf string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * A C program that prints a string.
+ * The main function contains a printf function,
+ * that prints the phrase,
+ * "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
