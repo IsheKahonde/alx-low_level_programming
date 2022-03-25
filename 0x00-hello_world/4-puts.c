40fe553e9f9f8e8b5ec731dd2ca00cef117aff9a
Ishe <ishekahonde@gmail.com>
2022-03-25 17:50:38 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 /**
  * A C program that prints the string,
- * "Programming is like building a multilingual puzzle."
- * The program uses the puts function
- * It terminates with a 0 if successful.
+ * main: "Programming is like building a multilingual puzzle."
+ * return: 0 if successful.
  */
 
 int main(void)
